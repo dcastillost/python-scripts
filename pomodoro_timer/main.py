660fb59520ab6952f31a1ae26ecb0ab9b966c088
David Castillo <dcastillo.st@gmail.com>
2020-11-14 16:20:57 -0600

    Complete Pomodoro app

--- pomodoro_timer/main.py
+++ pomodoro_timer/main.py
@@ -7,26 +7,61 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
+
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+# WORK_ITER = [1, 3, 5, 7]
+# SHORT_BREAK_ITER = [2, 4, 6]
+# LONG_BREAK_ITER = [8]
+
+timer = None
+timer_iter = 0
+
 # ---------------------------- TIMER RESET ------------------------------- # 
 def reset_timer():
-    pass
+    global timer_iter
+    window.after_cancel(timer)
+    timer_iter = 0
+    title_label.config(text='Timer', fg = GREEN)
+    canvas.itemconfig(timer_text, text='00:00')
+    checkmark_label.config(text='')
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    countdown(WORK_MIN*60)
+    global timer_iter
+    timer_iter += 1
+
+    work_sec = WORK_MIN*60
+    short_break_sec = SHORT_BREAK_MIN*60
+    long_break_sec = LONG_BREAK_MIN*60 
+
+    if timer_iter % 8 == 0:
+        title_label.config(text='Break', fg = RED)
+        countdown(long_break_sec)
+    elif timer_iter % 2 == 0:
+        title_label.config(text='Break', fg = PINK)
+        countdown(short_break_sec)
+    else:
+        title_label.config(text='Work', fg = GREEN)
+        countdown(work_sec)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def countdown(count):
+    global timer
     minutes = count // 60
     seconds = count % 60
-    canvas.itemconfig(timer_text, text='{:02d}:{:02d}'.format(minutes,seconds))
-    text=f''
+    canvas.itemconfig(timer_text, text='{:02d}:{:02d}'.format(minutes, seconds))
     if count > 0:
-        window.after(1000, countdown, count - 1)
+        timer = window.after(1000, countdown, count - 1)
+    else:
+        start_timer()
+        marks = ''
+        work_sessions = timer_iter // 2
+        for _ in range(work_sessions):
+            marks += '✔'
+        checkmark_label.config(text=marks)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -37,7 +72,7 @@ window.config(padx=100, pady=50, bg=YELLOW)
 title_label = Label(text='Timer', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, 'bold'))
 title_label.grid(row=0, column=1)
 
-checkmark_label = Label(text='✔', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20))
+checkmark_label = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20))
 checkmark_label.grid(row=3, column=1)
 
 # Buttons
