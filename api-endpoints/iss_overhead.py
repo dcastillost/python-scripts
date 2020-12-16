473fd1d12655f6b325ae0631c7226e7ea7cf9317
David Castillo <dcastillo.st@gmail.com>
2020-12-16 12:58:37 +0900

    Complete ISS project

--- api-endpoints/iss_overhead.py
+++ api-endpoints/iss_overhead.py
@@ -1,22 +1,54 @@
-import request
 from datetime import datetime
+import requests
+import smtplib
+import time
 
-URL = 'https://api.sunrise-sunset.org/json'
+from user_info import EMAIL, PASSWORD
+
+ISS_URL = 'http://api.open-notify.org/iss-now.json'
+SUNSET_SUNSET_URL = 'https://api.sunrise-sunset.org/json'
 MY_LAT = 25.569863
 MY_LONG = -103.362797
 
-parameters = {
-    'lat':  MY_LAT,
-    'lng': MY_LONG,
-    'formatted': 0,
-}
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
 
-#### FUNCTIONS ####
-def compare_location(my_location, iss_location, range=5):
-    """Check if the position of the ISS is inside the range of visualization"""
-    if iss_location['lat'] < my_location['lat'] + range \
-       and iss_location['lat'] > my_location['lat']-range \
-       :
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
 
-    my_location['lat']
-    my_location['lng']
+    #Your position is within +5 or -5 degrees of the iss position.
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+        return True
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+    response = requests.get(SUNSET_SUNSET_URL, params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        connection = smtplib.SMTP("__YOUR_SMTP_ADDRESS_HERE___")
+        connection.starttls()
+        connection.login(EMAIL, PASSWORD)
+        connection.sendmail(
+            from_addr=EMAIL,
+            to_addrs=EMAIL,
+            msg="Subject:Look Up👆\n\nThe ISS is above you in the sky."
+        )
